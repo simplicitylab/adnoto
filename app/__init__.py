4f47ed61855a4a576ecdb326022c92d5a6a3da81
Glenn De Backer <glenn@simplicity.be>
2014-12-25 14:11:06 +0100

    Basic skeleton app

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+# Import flask and template operators
+from flask import Flask, render_template
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+print app.config
