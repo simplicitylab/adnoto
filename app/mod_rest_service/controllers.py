5b9cbaf6b4c8a2fed228cd14c101d7a8bae2c428
Glenn De Backer <glenn@simplicity.be>
2014-12-25 20:55:26 +0100

    Implemented serializer marshmallow

--- app/mod_rest_service/controllers.py
+++ app/mod_rest_service/controllers.py
@@ -1,11 +1,14 @@
 # Import flask dependencies
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify
 
 # Import the database object from the main app module
 from app import db
 
-# Import module models
+# Import module models and schema Notebook, Page
 from app.mod_rest_service.models import Notebook
+from app.mod_rest_service.models import NotebookSchema
+
+from app.mod_rest_service.models import Page
 
 # Define the blueprint: 'rest_service', set its url prefix: app.url/api/v1
 mod_rest_service = Blueprint('rest_service', __name__, url_prefix='/api/v1')
@@ -15,3 +18,18 @@ mod_rest_service = Blueprint('rest_service', __name__, url_prefix='/api/v1')
 def test():
     # render test.html
     return render_template("rest/test.html")
+
+
+# List notebooks
+@mod_rest_service.route('/notebooks', methods=['GET'])
+def list_notebooks():
+
+    # get all notebooks
+    notebooks = Notebook.query.all()
+
+    # serialize sqlalchemy data
+    serializer = NotebookSchema(many=True)
+    result = serializer.dump(notebooks)
+
+    # return json result
+    return jsonify({"notebooks": result.data})
--- app/mod_rest_service/models.py
+++ app/mod_rest_service/models.py
@@ -2,9 +2,16 @@
 # this is defined inside /app/__init__.py
 from app import db
 
+# Import marshmallow serializer
+from marshmallow import Schema, fields, ValidationError
+
+##### Models #####
+
 # Define a base model for other database tables to inherit
 class Base(db.Model):
-
+    """
+    Base model that other models inherit from
+    """
     __abstract__  = True
 
     id            = db.Column(db.Integer, primary_key=True)
@@ -14,6 +21,9 @@ class Base(db.Model):
 
 # Define a Page model
 class Page(Base):
+    """
+    Describes a page in the notebook
+    """
     __tablename__ = 'page'
 
     # Page title
@@ -25,6 +35,9 @@ class Page(Base):
 
 # Define a Notebook model
 class Notebook(Base):
+    """
+    Describes a notebook model
+    """
     __tablename__ = 'notebook'
 
     # Notebook Name
@@ -34,4 +47,20 @@ class Notebook(Base):
     pages = db.relationship("Page")
 
 
+##### SCHEMAS #####
+
+# validator
+def must_not_be_blank(data):
+    if not data:
+        raise ValidationError('Data not provided.')
+
+# Notebook schema
+class NotebookSchema(Schema):
+    """
+    Schema for model Notebook
+    """
+    name = fields.Str(required=True, validate=must_not_be_blank)
+
+    class Meta:
+        fields = ("id", "date_created", "date_modified", 'name')
 
